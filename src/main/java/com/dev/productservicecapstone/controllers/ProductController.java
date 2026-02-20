5ee4a5bf7d8b0d4e8da21bb1fe74da4ab70445bd
ashishnagsforce <131547379+ashishnagsforce@users.noreply.github.com>
2026-02-20 11:45:20 +0530

    Added Exceptions,CreateProduct,GetProduct

--- src/main/java/com/dev/productservicecapstone/controllers/ProductController.java
+++ src/main/java/com/dev/productservicecapstone/controllers/ProductController.java
@@ -1,14 +1,17 @@
 package com.dev.productservicecapstone.controllers;
-
+import com.dev.productservicecapstone.dtos.CreateFakeStoreProductRequestDto;
 import com.dev.productservicecapstone.dtos.ProductResponseDto;
 import com.dev.productservicecapstone.exceptions.ProductNotFoundException;
 import com.dev.productservicecapstone.models.Product;
 import com.dev.productservicecapstone.services.ProductService;
+import com.dev.productservicecapstone.dtos.ErrorDto;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 
 import java.util.ArrayList;
 import java.util.List;
-
+import java.util.stream.Collectors;
 @RestController
 public class ProductController {
 
@@ -39,5 +42,36 @@ public class ProductController {
     }
 
     @PostMapping("/products")
-    public
+    public ResponseEntity<ProductResponseDto> createProduct(
+            @RequestBody CreateFakeStoreProductRequestDto
+                    createFakeStoreProductRequestDto)
+    {
+        Product product = productService.createProduct(
+                createFakeStoreProductRequestDto.getName(),
+                createFakeStoreProductRequestDto.getDescription(),
+                createFakeStoreProductRequestDto.getPrice(),
+                createFakeStoreProductRequestDto.getImageUrl(),
+                createFakeStoreProductRequestDto.getCategory()
+        );
+        return new ResponseEntity<>(ProductResponseDto.from(product), HttpStatus.CREATED);
+    }
+
+
+    @PutMapping("/products/{id}")
+    public ProductResponseDto replaceProduct(@PathVariable("id") long id,
+                                             @RequestBody CreateFakeStoreProductRequestDto
+                                                     createFakeStoreProductRequestDto)
+    {
+        Product product = productService.replaceProduct(
+                id,
+                createFakeStoreProductRequestDto.getName(),
+                createFakeStoreProductRequestDto.getDescription(),
+                createFakeStoreProductRequestDto.getPrice(),
+                createFakeStoreProductRequestDto.getImageUrl(),
+                createFakeStoreProductRequestDto.getCategory()
+        );
+
+        return ProductResponseDto.from(product);
+    }
+
 }
